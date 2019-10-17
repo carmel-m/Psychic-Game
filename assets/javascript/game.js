d0ba2c56feaaf8d0962879cde7c529d09e4a7b66
Carmel Meyer <cemeyer@elkera.com>
2019-10-17 17:34:19 -0400

    basic functionality added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,30 +6,39 @@ var guessesLeft = 9;
 var userGuesses = [];
 var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// generate random letter when window loads/refreshes/user wins/user loses
-var randomLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
-
-console.log(randomLetter);
-
-// take user input (typing a letter)
-document.onkeyup = function(event) {
-    //determine which key was pressed
-    var userInput = event.key; 
-    
-if (userInput == randomLetter) {
-
-}
-    
-
-
-};
-
-
-// these will update the html as the game progresses
+//displayed when page opens:
 document.getElementById("win-tally").textContent = wins;
 document.getElementById("loss-tally").textContent = losses;
 document.getElementById("guesses-left").textContent = guessesLeft;
 document.getElementById("your-guesses").textContent = userGuesses;
+
+
+// generate random letter when window loads/refreshes/user wins/user loses
+var randomLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
+console.log(randomLetter);
+
+// take user input (typing a letter)
+document.onkeyup = function(event) {
+  
+//determine which key was pressed
+var userInput = event.key; 
+    
+
+// if statement checking if letter matches and updating win tally
+if (userInput === randomLetter) {
+    wins++;
+    document.getElementById("win-tally").textContent = wins;
+} else {
+    userGuesses.push(userInput);
+    guessesLeft--; //not working??
+    document.getElementById("guesses-left").textContent = guessesLeft;
+    // console.log(guessesLeft); // console logging works though
+    document.getElementById("your-guesses").textContent = userGuesses;
+
+
+}
+    
+}
 
 
 
@@ -43,7 +52,7 @@ document.getElementById("your-guesses").textContent = userGuesses;
     // wins++
 
 
-
+// if guessesLeft reaches 0, add to loss and restart
 
 
 
